bbe9682ff88c282f0408fbc0d330011385c2b3b9
Nanne Wielinga <nanne@mycel.nl>
2022-03-12 13:41:32 +0100

    parallel query fix

--- frontend/src/sqlite.ts
+++ frontend/src/sqlite.ts
@@ -19,6 +19,7 @@ export class SQLJSDriver implements Driver {
     this.connection = new SQLJSConnection(buf);
   }
   init(): Promise<void> {
+    console.log('SQLJS driver initialized');
     return this.connection.opened;
   }
   acquireConnection(): Promise<DatabaseConnection> {
@@ -46,58 +47,99 @@ class SQLJSConnection implements DatabaseConnection {
   // db: Database;
   worker: Worker;
 
+  resolvers: Record<number, (result: QueryResult<any>) => void> = {};
+  rejecters: Record<number, (error: any) => void> = {};
+  id: number = 1;
+
   constructor(buf: Promise<ArrayBuffer>) {
     // this.worker = new Worker("sql.js/dist/worker.sql-wasm.js");
     this.worker = new Worker(new URL('/dist/worker.sql-wasm.js', import.meta.url), { type: "module" });
+    const _this = this;
+    this.worker.onmessage = (e) => {
+      if (!('data' in e) || !('id' in e.data)) {
+        return;
+      }
+      const id = e.data.id;
+      if (!id || 
+        !(id in _this.resolvers) || 
+        !(id in _this.rejecters) || 
+        !_this.rejecters[id] || 
+        !_this.resolvers[id]) {
+        throw new Error(`Unknown id: ${id}`);
+      }
+      if('error' in e.data) {
+        _this.rejecters[id]?.(e.data.error);
+      }
+      if('results' in e.data) {
+        if(e.data.results.length > 0) {
+          const columns = e.data.results[0].columns as string[];
+          const values = e.data.results[0].values;
+          _this.resolvers[id]?.(
+              {
+                rows: values.map((row: any) => 
+                  Object.fromEntries(columns.map((_, i) => [columns[i], row[i]]))
+                ),
+              }
+          )
+        } else {
+          _this.resolvers[id]?.({
+            rows: []
+          });
+        }
+      } else {
+        _this.resolvers[id]?.(e.data);
+      }
+    };
     this.opened = new Promise((resolve, reject) => {
+      _this.id++;
+      this.resolvers[_this.id] = (_result: QueryResult<any>) => {
+        resolve();
+      };
+      this.rejecters[_this.id] = (error: any) => {
+        reject(error);
+      };
       buf.then(buffer => {
         this.worker.postMessage({
-          id:1,
+          id: _this.id,
           action:"open",
           buffer
         });
       });
-      this.worker.onmessage = () => {
-        resolve();
-      }
-      this.worker.onerror = (e) => {
-        reject(e);
-      }
+      
+      this.worker.onerror = (e: any) => {
+        console.error(e);
+        if('data' in e && 'id' in e.data) {
+          const id = e.data.id;
+          if('error' in e.data && id in _this.rejecters && _this.rejecters[id]) {
+            _this.rejecters?.[id](e);
+          }
+        }
+      };
     });
   }
   opened: Promise<void>;
-  id: number = 0;
+  
+
   executeQuery<R>(compiledQuery: CompiledQuery): Promise<QueryResult<R>> {
     return new Promise<QueryResult<R>>((resolve, reject) => {
       const currentId = this.id++;
+      console.log("query", currentId , compiledQuery.sql);
+
+      this.resolvers[currentId] = (result: QueryResult<R>) => {
+        console.log("result", currentId, result);
+        resolve(result);
+      }
+      this.rejecters[currentId] = (error: any) => {
+        reject(error);
+      }
+    
       this.worker.postMessage({
         id: currentId,
         action: "exec",
         sql: compiledQuery.sql,
         params: compiledQuery.parameters
       });
-      this.worker.onmessage = (e) => {
-        if (e.data.id === currentId) {
-          if('error' in e.data) {
-            reject(e.data.error);
-          } else if(e.data.results.length > 0) {
-            const columns = e.data.results[0].columns as string[];
-            const values = e.data.results[0].values;
-            console.log(columns, values);
-            resolve(
-              {
-                rows: values.map((row: any) => 
-                  Object.fromEntries(columns.map((_, i) => [columns[i], row[i]]))
-                ),
-              }
-            )
-          } else {
-            resolve({
-              rows: []
-            });
-          }
-        }
-      }
+      
     });
   }
 
