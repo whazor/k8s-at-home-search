214e494cce942cc381eebc0512522c1784d37936
Nanne Wielinga <nanne@mycel.nl>
2022-08-02 20:18:44 +0200

    Remove unreliable test

--- frontend/e2e/search.spec.ts
+++ frontend/e2e/search.spec.ts
@@ -10,8 +10,6 @@ test('test search', async ({ page }) => {
   await page.locator('[placeholder="search a chart"]').fill('plex');
   await expect(page).toHaveURL('#/plex');
 
-  const lastModified = page.locator('td.last-modified');
-  await expect(lastModified).toHaveText(/days ago/);
 
   const links = await page.$$('a:has-text("plex")');
   expect(links).not.toHaveLength(0)
