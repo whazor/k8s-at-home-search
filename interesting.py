21232cd9f9268ae24061ea3d55fec9145188acba
Nanne Wielinga <nanne@mycel.nl>
2022-08-20 19:11:40 +0200

    Deprecate awesome-home-kubernetes json, add pagination
    Fixes #2

--- interesting.py
+++ interesting.py
@@ -7,44 +7,27 @@ github_header = {'content-type': 'application/json'}
 if 'GITHUB_TOKEN' in os.environ:
     github_header['Authorization'] = 'Bearer ' + os.environ['GITHUB_TOKEN']
 
+repos = set()
 results = []
 
-
-url = "https://api.github.com/search/repositories?q=topic:k8s-at-home&per_page=100"
-
-items = requests.get(url, headers=github_header).json()['items']
-repos = set()
-for repo_info in items:
-    repo_name = repo_info['full_name']
-    stars = repo_info['stargazers_count']
-    url = repo_info['html_url']
-    branch = repo_info['default_branch']
-    results.append((repo_name, url, branch, stars))
-    repos.add(repo_name)
-
-
-awesome_repos = set()
-repos_url = "https://raw.githubusercontent.com/k8s-at-home/awesome-home-kubernetes/main/data.json"
-data = requests.get(repos_url).json()
-for repo in data['user_repositories']:
-    # if flux
-    if 'gitops_tool' in repo and repo['gitops_tool'] == 'flux':
-        name = repo['repo']
-        awesome_repos.add(name)
-
-
-for given_repo_name in awesome_repos:
-    if given_repo_name not in repos:
-        repo_info = requests.get('https://api.github.com/repos/'+given_repo_name, headers=github_header).json()
-        if 'stargazers_count' in repo_info and 'default_branch' in repo_info:
-            repo_name = repo_info['full_name']
-            if repo_name not in repos:
-                repos.add(repo_name)
-                stars = repo_info['stargazers_count']
-                branch = repo_info['default_branch']
-                url = repo_info['html_url']
-                # insert or update
-                results.append((repo_name, url, branch, stars))
+items = []
+page = 1
+while len(items) == 100 or page == 1:
+    items = requests.get("https://api.github.com/search/repositories",
+            params={
+                'q': 'topic:k8s-at-home',
+                'per_page': 100,
+                'page': page
+            },
+            headers=github_header).json()['items']
+    for repo_info in items:
+        repo_name = repo_info['full_name']
+        stars = repo_info['stargazers_count']
+        url = repo_info['html_url']
+        branch = repo_info['default_branch']
+        results.append((repo_name, url, branch, stars))
+        repos.add(repo_name)
+    page += 1
 
 if len(results) < 50:
     print("Not enough repos, error fetching topic github repos")
