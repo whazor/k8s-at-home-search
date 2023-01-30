1b139fb5472241311f165669aff9beac1a3e3557
Nanne Wielinga <nanne@mycel.nl>
2023-01-30 22:11:22 +0100

    add stars back

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
-import { Link, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import HelmRelease from './pages/helm-release';
 import Home from './pages/index';
 import styles from "./index.css?inline"
 import { SearchBar } from './components/search';
 import { AppData as HRAppData, denormalize } from './generators/helm-release/models';
+import GitHubButton from 'react-github-btn';
 
 export type AppData = HRAppData;
 
@@ -14,6 +15,9 @@ export default function App(props: AppData & { pageData: any }) {
     <div className='p-4 dark:bg-gray-900'>
       <style>{styles}</style>
       <nav>
+      <div className="float-right p-4 pt-0">
+        <GitHubButton href="https://github.com/whazor/k8s-at-home-search" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star whazor/k8s-at-home on GitHub">Star</GitHubButton>
+      </div>
         <a
         href={'/k8s-at-home-search/'} 
         ><h1 className="text-3xl dark:text-white">k8s at home search</h1></a>
