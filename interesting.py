b6fa9c4d48a527ae18184cb4e206fcfa8284dce8
Nanne Wielinga <184182+whazor@users.noreply.github.com>
2023-04-27 13:03:23 +0200

    Add graphql query and extra repo checks

--- interesting.py
+++ interesting.py
@@ -6,7 +6,9 @@ from datetime import datetime
 github_header = {"content-type": "application/json"}
 if "GITHUB_TOKEN" in os.environ:
     github_header["Authorization"] = "Bearer " + os.environ["GITHUB_TOKEN"]
-
+else:
+    print("Missing GITHUB_TOKEN")
+    exit(1)
 repos = set()
 results = []
 
@@ -25,7 +27,7 @@ while len(items) > 0 or page == 1:
             continue
         repo_name = repo_info["full_name"]
         if "true_charts" in repo_name:
-            # Skip true_charts, only charts no helm_release
+            # Skip true_charts because they do not have helm_release to parse
             continue
         stars = repo_info["stargazers_count"]
         url = repo_info["html_url"]
@@ -33,6 +35,78 @@ while len(items) > 0 or page == 1:
         results.append((repo_name, url, branch, stars))
         repos.add(repo_name)
     page += 1
+
+# graphql query to get all repos from github, as their api is unreliable
+def run_graphql_query(variables):
+    query = """
+    query ($cursor: String) {
+      topic(name: "k8s-at-home") {
+        repositories(first: 100, after: $cursor) {
+          pageInfo {
+            endCursor
+            startCursor
+          }
+          nodes {
+            id
+            nameWithOwner
+            stargazerCount
+            url
+            defaultBranchRef {
+              name
+            }
+            pushedAt
+          }
+        }
+      }
+    }
+"""
+    payload = {"query": query, "variables": variables}
+    response = requests.post("https://api.github.com/graphql", json=payload, headers=github_header)
+    return response.json()
+
+cursor = None
+has_next_page = True
+while has_next_page:
+    variables = {"cursor": cursor}
+    result = run_graphql_query(variables)
+    nodes = result["data"]["topic"]["repositories"]["nodes"]
+    for repo in nodes:
+        pushed_at = datetime.strptime(repo["pushedAt"], "%Y-%m-%dT%H:%M:%SZ")
+        # filter out unmaintained repos
+        if (datetime.now() - pushed_at).days > 90:
+            continue
+        repo_name = repo["nameWithOwner"]
+        if "true_charts" in repo_name:
+            # Skip true_charts because they do not have helm_release to parse
+            continue
+        stars = repo["stargazerCount"]
+        url = repo["url"]
+        branch = repo["defaultBranchRef"]["name"]
+        # check if repo is already in results
+        if not repo_name in repos:
+            results.append((repo_name, url, branch, stars))
+            repos.add(repo_name)
+    has_next_page = result["data"]["topic"]["repositories"]["pageInfo"]["endCursor"]
+    cursor = result["data"]["topic"]["repositories"]["pageInfo"]["endCursor"]
+
+# check if all must_have repos are in results
+must_have = {
+    "onedr0p/home-ops",
+    "billimek/k8s-gitops",
+    "xUnholy/k8s-gitops",
+    "bjw-s/home-ops",
+    "wrmilling/k3s-gitops",
+    "carpenike/k8s-gitops",
+    "truxnell/home-cluster",
+    "brettinternet/homelab",
+    "angelnu/k8s-gitops",
+    "anthr76/infra"
+}
+
+for repo in must_have:
+    if repo not in repos:
+        print(f"Missing {repo}")
+        exit(1)
 
 if len(results) < 50:
     print("Not enough repos, error fetching topic github repos")
