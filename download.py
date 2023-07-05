40dc68d4376dbb17a9c06ef9f01b9e49fcdc9a3a
Nanne Wielinga <184182+whazor@users.noreply.github.com>
2023-07-05 19:39:52 +0200

    script improvements

--- download.py
+++ download.py
@@ -1,18 +1,9 @@
-# #!/bin/bash
-# git submodule init
-# # add as submodule
-# eval "$(sqlite3 repos.db 'select "git submodule add " ||url || " repos/" || REPLACE(repo_name, "/", "-") from repos')"
-
-# # git config pull.ff only
-# # find repos -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull \;
-# git submodule foreach git config --local pull.ff only
-# git submodule foreach git pull 
 from re import sub
 import subprocess
 import sqlite3
 
 def bash_command(cmd):
-    subprocess.Popen(['/bin/bash', '-c', cmd])
+    subprocess.Popen(['bash', '-c', cmd])
 
 conn = sqlite3.connect('repos.db')
 c = conn.cursor()
--- scripts/download.mjs
+++ scripts/download.mjs
@@ -29,8 +29,8 @@ await $`pigz -d -k repos.db.zz`;
 await $`pigz -d -k repos-extended.db.zz`;
 
 // move to web/
-await $`mv repos.db ./web/`;
-await $`mv repos-extended.db ./web/`;
+await $`cp repos.db ./web/`;
+await $`cp repos-extended.db ./web/`;
 
 await $`rm -f repos.db.zz`;
 await $`rm -f repos-extended.db.zz`;
