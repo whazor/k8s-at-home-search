fa70436f8e88424a1a910ca4775ebd7a04ea9b78
Nanne <184182+whazor@users.noreply.github.com>
2025-01-15 21:10:22 +0100

    bump dependencies

--- flake.nix
+++ flake.nix
@@ -1,53 +1,60 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    devenv.url = "github:cachix/devenv";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    devshell.url = "github:numtide/devshell";
+
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
-        inputs.devenv.flakeModule
+        inputs.devshell.flakeModule
       ];
-      systems =  [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
       perSystem = { config, self', inputs', pkgs, lib, system, ... }:
-      let
-      nodejs = pkgs.nodejs_20;
-      python = pkgs.python311;
-      in
-      {
-        packages.default = pkgs.hello;
-        devenv.shells.default = {
-          # https://devenv.sh/reference/options/
-
-          languages.nix.enable = true;
-          languages.javascript.enable = true;
-          languages.javascript.package = nodejs;
-          languages.typescript.enable = true;
-
-          languages.python.enable = true;
-          languages.python.package = python.withPackages (p: with p; [
-            requests
-            ruamel-yaml
-            pydantic
-          ]);
-
-
-          packages = [
-            config.packages.default
-            pkgs.sqlite
-            pkgs.playwright-driver
-            pkgs.nodePackages.zx
-          ];
-
-          env.PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
-          env.PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
-
-          processes.web.exec = "cd $DEVENV_ROOT/web/; yarn run dev";
-          scripts.download.exec = "$DEVENV_ROOT/scripts/download.mjs";
-
+        let
+          nodejs = pkgs.nodejs_23;
+          python = pkgs.python3;
+        in
+        {
+          devshells.default = {
+            env = [
+              {
+                name = "BROWSER_PATH";
+                value = pkgs.playwright-driver.browsers;
+              }
+              {
+                name = "PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS";
+                value = "true";
+              }
+            ];
+            commands = [
+              {
+                help = "download repos";
+                name = "download";
+                command = "./scripts/download.mjs";
+              }
+              {
+                help = "run frontend";
+                name = "run";
+                command = "cd web/; yarn run dev";
+              }
+            ];
+            packages = [
+              (python.withPackages (p: with p; [
+                requests
+                ruamel-yaml
+                pydantic
+              ]))
+              pkgs.sqlite
+              pkgs.curl
+              pkgs.playwright-driver
+              pkgs.nodePackages.zx
+              pkgs.nodePackages.npm
+              pkgs.nodePackages.yarn
+            ];
+          };
         };
-      };
     };
 }
--- scripts/download.mjs
+++ scripts/download.mjs
@@ -2,24 +2,60 @@
 
 $.verbose = false;
 
+let date = null;
+
+// if argument, then it is a date
+if (process.argv[3]) {
+    date = process.argv[3];
+    console.log("using date", date);
+}
+
+
 // download latest releases from github
 const repo = 'whazor/k8s-at-home-search';
 // use $`curl` for download
-const { stdout } = await $`curl -s https://api.github.com/repos/${repo}/releases`;
+const { stdout } = await $`curl -s https://api.github.com/repos/${repo}/releases?per_page=100`;
+
+const sortedReleases = JSON.parse(stdout).map(r => r.name).sort();
+const firstName = sortedReleases[0];
+const lastName = sortedReleases[sortedReleases.length - 1];
+const monthOldName = sortedReleases.length > 33 && sortedReleases[sortedReleases.length - 32];
 
 const releases = JSON.parse(stdout);
+// print first and last release
+console.log(`from release: ${firstName}, to latest release: ${lastName}, one month ago: ${monthOldName}`);
 
-// get latest release
-const latestName = releases.map(({ name }) => name).sort().reverse()[0];
-const latest = releases.find(({ name }) => name === latestName);
-console.log(`latest release: ${latest.name}`);
+
+
+let release;
+if (date) {
+    release = releases.find(({ name }) => name.includes(date));
+} else {
+    release = releases.find(({ name }) => name === lastName);
+}
+if (!release) {
+    console.error("no release found");
+    process.exit(1);
+}
+console.log(`found release: ${release.name}`);
 
 // parse json
-const { assets } = latest;
+const { assets } = release;
 
 const repos = assets.find(({ name }) => name === 'repos.db')["browser_download_url"];
 const reposExtended = assets.find(({ name }) => name === 'repos-extended.db')["browser_download_url"];
 
+console.log(`curl -s -L ${repos} -o ./web/repos${
+    date ? `-${date}` : ""
+}.db`);
+console.log(`curl -s -L ${reposExtended} -o ./web/repos-extended${
+    date ? `-${date}` : ""
+}.db`);
+
 // download files
-await $`curl -s -L ${repos} -o ./web/repos.db`;
-await $`curl -s -L ${reposExtended} -o ./web/repos-extended.db`;
+await $`curl -s -L ${repos} -o ./web/repos${
+    date ? `-${date}` : ""
+}.db`;
+await $`curl -s -L ${reposExtended} -o ./web/repos-extended${
+    date ? `-${date}` : ""
+}.db`;
